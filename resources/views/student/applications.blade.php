9fefda02ffae24949e89daac519ffe9434a1d31a
techcodedu <techcode.edu@gmail.com>
2023-04-16 16:49:38 +0800

    Updated feedback and student application view

--- resources/views/student/applications.blade.php
+++ resources/views/student/applications.blade.php
@@ -7,54 +7,57 @@
         <div class="col-md-6">
             <h4>Course Enrollments</h4>
             <table class="table table-striped table-bordered table-hover">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Course Name</th>
-                        <th>Enrollment Type</th>
-                        <th>Status</th>
-                         <th>Training Schedule</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse($enrollments as $enrollment)
-                        <tr>
-                                <td>{{ $enrollment->course->name }}</td>
-                                <td>{{ $enrollment->enrollment_type }}</td>
-                                <td>
-                                <span class="badge badge-{{ $enrollment->status === 'enrolled' ? 'success' : 'warning' }}">
-                                    {{ ucfirst($enrollment->status) }}
-                                </span>
-                                </td>
-                                <td>
-                                    @if(!empty($enrollment->trainingSchedule))
-                                         <button type="button" class="btn btn-sm btn-primary mt-2" onclick="viewTrainingSchedule({{ $enrollment->trainingSchedule->id }}, '{{ $enrollment->scholarship_grant }}')">View Schedule</button>
+    <thead class="thead-dark">
+        <tr>
+            <th>Course Name</th>
+            <th>Enrollment Type</th>
+            <th>Status</th>
+            <th>Training Schedule</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse($enrollments as $enrollment)
+            <tr>
+                <td>{{ $enrollment->course->name }}</td>
+                <td>{{ $enrollment->enrollment_type }}</td>
+                <td>
+                    <span class="badge badge-{{ $enrollment->status === 'enrolled' ? 'success' : 'warning' }}">
+                        {{ ucfirst($enrollment->status) }}
+                    </span>
+                </td>
+                <td>
+                    @if($enrollment->status === 'enrolled' && !empty($enrollment->trainingSchedule))
+                        <i class="fas fa-calendar-alt" style="cursor:pointer" onclick="viewTrainingSchedule({{ $enrollment->trainingSchedule->id }}, '{{ $enrollment->scholarship_grant }}')"></i>
+                    @else
+                        <span>-</span>
+                    @endif
+                    @if ($enrollment->feedback)
+                        <i class="fas fa-comments" style="cursor:pointer" onclick="openFeedbackViewModal({{ $enrollment->id }})" title="Read Feedback"></i>
+                    @else
+                        <span>No feedback yet</span>
+                    @endif
+                </td>
+                <!-- Update the Course Enrollments Actions -->
+                <td>
+                    @if ($enrollment->created_at->diffInDays(\Carbon\Carbon::now()) <= 3 && $enrollment->status !== 'enrolled')
+                        <a href="{{ route('enrollments.cancel', $enrollment->id) }}" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to cancel this enrollment?')">Cancel Enrollment</a>
+                    @elseif ($enrollment->status === 'enrolled')
+                        <button class="btn btn-success btn-sm" disabled>Vetted for Scholarship</button>
+                    @else
+                        <button class="btn btn-danger btn-sm" disabled>Cancel Enrollment</button>
+                    @endif
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="4" class="text-center">No enrollments found.</td>
+            </tr>
+        @endforelse
+    </tbody>
+</table>
 
-                                    @else
-                                        <span>No schedule yet</span>
-                                    @endif
-                                </td>
 
-                        <!-- Update the Course Enrollments Actions -->
-                            <td>
-                                @if ($enrollment->created_at->diffInDays(\Carbon\Carbon::now()) <= 3 && $enrollment->status !== 'enrolled')
-                                    <a href="{{ route('enrollments.cancel', $enrollment->id) }}" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to cancel this enrollment?')">Cancel Enrollment</a>
-                                @elseif ($enrollment->status === 'enrolled')
-                                    <button class="btn btn-success btn-sm" disabled>Vetted for Scholarship</button>
-                                @else
-                                    <button class="btn btn-danger btn-sm" disabled>Cancel Enrollment</button>
-                                @endif
-                            </td>
-
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="4" class="text-center">No enrollments found.</td>
-                        </tr>
-                    @endforelse
-                </tbody>
-
-            </table>
              <p class="text-muted mt-2">
                 <small>* Cancellation of enrollments is only allowed within 3 days of enrollment.</small>
             </p>
@@ -118,6 +121,7 @@
         </div>
     </div>
 </div>
+{{-- training schedule --}}
 <div class="modal fade" id="trainingScheduleModal" tabindex="-1" role="dialog" aria-labelledby="trainingScheduleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -136,6 +140,27 @@
         </div>
     </div>
 </div>
+{{-- view feeback --}}
+<!-- Feedback View Modal -->
+<div class="modal fade" id="feedbackViewModal" tabindex="-1" role="dialog" aria-labelledby="feedbackViewModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="feedbackViewModalLabel">Feedback Message</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p id="feedbackMessage"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <script>
     function viewTrainingSchedule(scheduleId, scholarshipGrant) {
@@ -150,6 +175,22 @@
 
         // Show the modal
         $('#trainingScheduleModal').modal('show');
+    });
+}
+
+</script>
+<script>
+    function openFeedbackViewModal(enrollmentId) {
+    $.ajax({
+        url: '/student/applications/view-feedback/' + enrollmentId,
+        method: 'GET',
+        success: function(response) {
+            $('#feedbackMessage').text(response);
+            $('#feedbackViewModal').modal('show');
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('Error:', textStatus, errorThrown);
+        }
     });
 }
 
