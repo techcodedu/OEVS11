9e91f6635a8a56306ba9eaae11371f3273aa6ad6
techcodedu <techcode.edu@gmail.com>
2023-04-17 22:36:02 +0800

    Added Analytics and Reports

--- app/Http/Controllers/Reports.php
+++ app/Http/Controllers/Reports.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Enrollment;
 use App\Models\StudentAssessment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Course;
+
 
 class Reports extends Controller
 {
@@ -34,6 +37,33 @@ class Reports extends Controller
         $totalCompleted = Enrollment::where('status', 'completed')->count();
         $totalInProgress = Enrollment::where('status', 'inProgress')->count();
 
-        return view('admin.reports', compact('enrollments', 'scheduledAssessments', 'totalEnrollments', 'totalCompleted', 'totalInProgress'));
+        $enrollmentStatusCount = Enrollment::select('status', DB::raw('count(*) as count'))->groupBy('status')->get();
+
+       $enrollmentCourseData = Course::leftJoin('enrollments', 'courses.id', '=', 'enrollments.course_id')
+        ->select('courses.name', DB::raw('count(enrollments.id) as enrollments_count'))
+        ->groupBy('courses.id', 'courses.name')
+        ->get()
+        ->map(function ($course) {
+            return [
+                'course' => $course->name,
+                'enrollments' => $course->enrollments_count
+            ];
+        });
+
+        // Count the enrollment types (scholarship and regular_training)
+        $enrollmentTypeData = Enrollment::select('enrollment_type', DB::raw('count(*) as count'))
+            ->groupBy('enrollment_type')
+            ->get();
+
+        // Count the student assessments by their remarks (Completed and Not Competent)
+        $assessmentRemarksData = StudentAssessment::select('remarks', DB::raw('count(*) as count'))
+            ->whereNotNull('schedule_date')
+            ->groupBy('remarks')
+            ->get();
+
+       // Pass the data to the view
+        return view('admin.reports', compact('enrollments', 'scheduledAssessments', 'totalEnrollments', 'totalCompleted', 'totalInProgress', 'enrollmentStatusCount', 'enrollmentCourseData', 'enrollmentTypeData', 'assessmentRemarksData'));
     }
+
+        
 }
--- resources/views/admin/reports.blade.php
+++ resources/views/admin/reports.blade.php
@@ -7,15 +7,83 @@
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1 class="m-0">{{ __('Reports') }}</h1>
+                    <!-- Add this code somewhere in your blade view to print the content of the $enrollmentCourseData variable -->
+
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
-
     <!-- Main content -->
     <div class="content">
         <div class="container-fluid">
+            <!-- Chart and analytics cards -->
+            <!-- Chart and analytics cards -->
+               <!-- Chart and analytics cards -->
+            <div class="row">
+                <!-- Chart card -->
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <canvas id="enrollmentStatusChart"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <!-- Line chart card -->
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <canvas id="enrollmentCoursesChart"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <!-- Assessment Remarks Bar Chart Card -->
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <canvas id="assessmentRemarksChart"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $totalEnrollments }}</h3>
+                            <p>Total Enrollments</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-user-check"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{ $totalCompleted }}</h3>
+                            <p>Completed Enrollments</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-graduation-cap"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>{{ $totalInProgress }}</h3>
+                            <p>Enrollments In Progress</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-spinner"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Filters -->
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
@@ -61,6 +129,7 @@
                             </div>
                         </form>
                     </div>
+
                     {{-- data in the table --}}
                     <div class="card">
                         <div class="card-header">
@@ -97,50 +166,13 @@
                             <button class="btn btn-primary" onclick="printTable('enrollmentsTable')">Print</button>
                         </div>
                     </div>
-                      {{-- end of table card --}} 
-                        {{-- Analytics Section --}}
-                    <div class="row">
-                        <div class="col-lg-4">
-                            <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3>{{ $totalEnrollments }}</h3>
-                                    <p>Total Enrollments</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fas fa-user-check"></i>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="small-box bg-success">
-                                <div class="inner">
-                                    <h3>{{ $totalCompleted }}</h3>
-                                    <p>Completed Enrollments</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fas fa-graduation-cap"></i>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="small-box bg-warning">
-                                <div class="inner">
-                                    <h3>{{ $totalInProgress }}</h3>
-                                    <p>Enrollments In Progress</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fas fa-spinner"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {{-- End of Analytics Section --}}
-
+                      {{-- end of table card --}}
                 </div>
             </div>
             <!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         function printTable(tableId) {
             const printWindow = window.open('', '_blank');
@@ -152,8 +184,97 @@
             printWindow.print();
         }
     </script>
+    <script>
+    // Enrollment Courses Line Chart
+    const enrollmentCourseData = @json($enrollmentCourseData);
+    const courseLabels = enrollmentCourseData.map(e => e.course);
+    const enrollmentCounts = enrollmentCourseData.map(e => e.enrollments);
+
+    const lineCtx = document.getElementById('enrollmentCoursesChart').getContext('2d');
+    const enrollmentCoursesChart = new Chart(lineCtx, {
+    type: 'line',
+    data: {
+        labels: courseLabels,
+        datasets: [{
+            label: 'Enrollments per Course',
+            data: enrollmentCounts,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        maintainAspectRatio: false,
+        responsive: true
+    }
+});
+
+</script>
+<script>
+    // Enrollment Types Pie Chart
+    const enrollmentTypeData = @json($enrollmentTypeData);
+    const enrollmentTypeLabels = enrollmentTypeData.map(e => e.enrollment_type);
+    const enrollmentTypeCounts = enrollmentTypeData.map(e => e.count);
+
+    const pieCtx = document.getElementById('enrollmentStatusChart').getContext('2d');
+    const enrollmentStatusChart = new Chart(pieCtx, {
+        type: 'pie',
+        data: {
+           labels: enrollmentTypeLabels,
+            datasets: [{
+                data: enrollmentTypeCounts,
+                backgroundColor: ['#FF6384', '#36A2EB'],
+                hoverBackgroundColor: ['#FF6384', '#36A2EB']
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            responsive: true
+        }
+    });
+
+    // Student Assessment Remarks Bar Chart
+    const assessmentRemarksData = @json($assessmentRemarksData);
+    const assessmentRemarksLabels = assessmentRemarksData.map(e => e.remarks);
+    const assessmentRemarksCounts = assessmentRemarksData.map(e => e.count);
+
+    const barCtx = document.getElementById('assessmentRemarksChart').getContext('2d');
+    const assessmentRemarksChart = new Chart(barCtx, {
+        type: 'bar',
+        data: {
+            labels: assessmentRemarksLabels,
+            datasets: [{
+                label: 'Student Assessments by Remarks',
+                data: assessmentRemarksCounts,
+                backgroundColor: ['#42A5F5', '#66BB6A'],
+                borderColor: ['#42A5F5', '#66BB6A'],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                },
+                x: {
+                    barPercentage: 0.1, // Controls the width of individual bars (0 to 1)
+                    categoryPercentage: 0.5 // Controls the width of the category containing the bars (0 to 1)
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+        }
+    });
+</script>
+
+
 
 <!-- /.content -->
 @endsection
 
-                                           
+
